26f87b538b9d3d631ed900a44742b1cadf35d650
Anita Ganene <anitaganenekulungu@gmail.com>
2024-03-25 14:36:48 +0200

    responsivite sur la page noslieux

--- server/app.js
+++ server/app.js
@@ -3,8 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.use(express.json())
-app.use(express.urlencoded({extended : false}))
+// app.use(express.json())
+// app.use(express.urlencoded({extended : false}))
 
 
 
