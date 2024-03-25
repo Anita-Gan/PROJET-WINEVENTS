e6c879d0a09f5b731b6b0ca642a73a07a4c30add
Anita Ganene <anitaganenekulungu@gmail.com>
2024-03-25 12:02:01 +0200

    installation des prisma dans le backend

--- client/src/menu/sevices.jsx
+++ client/src/menu/sevices.jsx
@@ -3,7 +3,6 @@ import Footer from "../composant/footer";
 export default function Services() {
     return (
         <div className="service">
-            {/* <img src="/images/cadre.jpg" alt="" /> */}
             <div className="service-text">
                 <h2>Services</h2>
             </div>
